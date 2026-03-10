94ee02944d37e0aa63589c097de4f75eba047fa6
Mirt12 <Mirt12@yandex.ru>
2026-03-10 23:13:25 +0100

    add TradingPage class, refactored UITradingTest class

--- src/test/java/ui/UITradingTest.java
+++ src/test/java/ui/UITradingTest.java
@@ -1,29 +1,38 @@
 package ui;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
+import java.util.function.Function;
 
-public class UILoginFormTest {
+public class UITradingTest {
 
     ChromeOptions options = new ChromeOptions();
 
     @Test
-    public void isHomePageLoaded(){
+    public void isTradingPageOpen() {
         options.addArguments("--incognito");
         options.addArguments("--disable-cache");
-        ChromeDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://capital.com");
-        //fluent Wait- Set up:
         Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-                .withTimeout(Duration.ofSeconds(30L))
-                .pollingEvery(Duration.ofSeconds(5L))
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
                 .ignoring(NoSuchElementException.class);
+        WebElement cookieBtn = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath("//button[@id='onetrust-reject-all-handler']"));
+            }
+        });
+        cookieBtn.click();
     }
 }
