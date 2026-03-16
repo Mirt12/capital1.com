bd3ca4b4b28247d004309c356bbaaec4e97aba15
Mirt12 <Mirt12@yandex.ru>
2026-03-17 00:30:40 +0100

    add userCanLogin() Test

---
+++ src/main/java/ui/steps/AutorizedSteps.java
@@ -0,0 +1,24 @@
+package ui.steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import ui.driver.DriverSingleton;
+import ui.trading.TradingPage;
+import ui.utils.LoadHelper;
+
+public class AutorizedSteps {
+    public WebDriver driver;
+
+    public AutorizedSteps() { this.driver = DriverSingleton.getDriver(); }
+
+    public void fillLoginFormAndSubmit(String email, String password) {
+        TradingPage page = new TradingPage();
+        Wait<WebDriver> wait = LoadHelper.wait30seconds();
+        page.getBaseURL()
+                .closeCookieModal(wait)
+                .clickAuthorizationLink(wait)
+                .fillInputFieldEmail(wait, email)
+                .fillInputFieldPassword(password)
+                .clickSubmitButtonOfLoginForm();
+    }
+}
